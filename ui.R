90c4be1b6b2c14e45739dcad454c882d50dedb5d
Hyewon Kim <kim5759@uw.edu>
2017-05-26 13:40:16 -0700

    Created tabpanel

---
+++ ui.R
@@ -0,0 +1,5 @@
+shinyUI(navbarPage("2015 Flight Delays and Cancellations",
+                   tabPanel("Component 1"),
+                   tabPanel("Component 2"),
+                   tabPanel("Component 3")
+))
