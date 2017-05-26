a9c3e9e00972b17ce141ce564cd10251d7d864c5
Hyewon Kim <kim5759@uw.edu>
2017-05-26 13:50:00 -0700

    To check

--- ui.R
+++ ui.R
@@ -1,5 +1,5 @@
 shinyUI(navbarPage("2015 Flight Delays and Cancellations",
-                   tabPanel("Component 1"),
+                   tabPanel("Intro"),
                    tabPanel("Component 2"),
                    tabPanel("Component 3")
 ))
